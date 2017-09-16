1d08181d230becf293ecfe554a13a5ad151eb52b
LaCRoSS <31204339+TarPakawat@users.noreply.github.com>
2017-09-16 17:14:01 +0700

    Update Master_mind.py

--- Master_mind.py
+++ Master_mind.py
@@ -1,3 +1,10 @@
+def conv(n):
+    n = str(n)
+    dic = {'1':"one" , '2':"two" , 
+        '3':"three" , '4':"four" , 
+        '5':"five" , '6':"six" , 
+        '7':"seven" , '8':"eight" , 
+        '9':"nine" , '0':""}
 ans = str(input("Enter a target (4-digit integer): "))
 ans = '0'*(4-len(ans)) + ans        #Add 0 if ans < 1000
 g = -1
@@ -10,8 +17,7 @@ if (g != ans):
             x += 1
         elif g[i] in ans:
             o += 1
-            if x != 4:
-             print('%d position correct, %d digits correct' % (x,o))
-            if x == 4:
-             print('Congratulations, you just mastered my mind!!')
-
+    if x != 4:
+     print('%d position correct, %d digits correct' % (x,o))
+    if x == 4:
+     print('Congratulations, you just mastered my mind!!')
