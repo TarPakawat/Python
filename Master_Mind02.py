beb5ab56e514aedbd94d3a1d32e642c664334a87
LaCRoSS <31204339+TarPakawat@users.noreply.github.com>
2017-09-17 13:29:07 +0700

    Create Master_Mind02.py

---
+++ Master_Mind02.py
@@ -0,0 +1,27 @@
+def conv(n):
+    n = str(n)
+    dic = {'1':"one" , '2':"two" , 
+        '3':"three" , '4':"four" , 
+        '5':"five" , '6':"six" , 
+        '7':"seven" , '8':"eight" , 
+        '9':"nine" , '0':"No"}
+    return dic[n]
+def firstChar(s):
+    s = s[0].upper() + s[1::len(s)]
+    return s
+ans = str(input("Enter a target (4-digit integer): "))
+ans = '0'*(4-len(ans)) + ans        #Add 0 if ans < 1000
+g = -1
+if (g != ans):
+    x = 0
+    o = 0
+    g = input("Enter your guess (4-digit integer): ")
+    for i in range(0,min(len(ans),len(g))):
+        if g[i] == ans[i]:
+            x += 1
+        if g[i] in ans and g[i] != ans:
+            o += 1
+    if x != 4:
+     print('%s position correct, %s digits correct' % (firstChar(conv(x)),conv(o)))
+    if x == 4:
+     print('Congratulations, you just mastered my mind!!')
