f134c0c8b06a4dd25f6914f35d94f540b106199e
LaCRoSS <31204339+TarPakawat@users.noreply.github.com>
2017-09-25 18:21:08 +0700

    Update Alphabet.py

--- Alphabet.py
+++ Alphabet.py
@@ -1,10 +1,18 @@
-x = int(input("Enter a number: "))
-i = 0
+a = int(input("Enter a number: "))
+count = 0
+loops = 0
 
-if not 0 < x <= 26:
+if a <= 0 or a > 26:
     print("Invalid input, program terminates.")
-else :
-    while i <= 26:
-        while i < x:
-           print(chr(ord('A')+i))    
-           i += 1 
+else:
+    while count < a:
+        loops = a-(count+1)
+        loop = 0
+        C = ''
+        while loops < a:
+            Chr = (chr((65+loop)))
+            loop += 1
+            loops += 1
+            C += Chr
+        print(C)
+        count += 1
